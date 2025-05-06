7cdf83c492f20c87c68e7a073f1b8f429b8a6747
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-05-06 14:04:10 -0300

    Rebrand to Kahon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "app.mihon"
+        applicationId = "com.amanoteam.kahon"
 
         versionCode = 27
         versionName = "0.20.2"
