05cfab57adeb6ef3bf88f136ab96f5927c83bcf7
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-10-30 19:30:50 -0300

    Exclude OSGI files from packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ android {
                 "META-INF/LICENSE",
                 "META-INF/NOTICE",
                 "META-INF/README.md",
+                "META-INF/versions/*/OSGI-INF/MANIFEST.MF"
             )
         }
     }
